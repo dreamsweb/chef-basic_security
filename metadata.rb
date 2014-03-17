1ddfb9dbb8b0b54cf8271e0fc9c004fc657c2d35
Nick Chernyshev <nick.chernyshev@gmail.com>
2014-03-17 13:30:47 +0600

    init

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "chef-basic-security"
+maintainer        "Dwwd"
+maintainer_email  "info@dwwd.ru"
+description       "Locks down a basic Ubuntu Install. E.g. remove SSH password & root login etc"
+version           "0.0.1"
+
+recipe "dwwd-basic_security", "Locks down a basic Ubuntu Install"
+
+supports "ubuntu"
